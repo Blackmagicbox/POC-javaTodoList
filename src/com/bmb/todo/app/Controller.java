817eef058a3cc0cfe4ff3838f39fceef166b203c
Rafael <rafa@blackmagicbox.com.br>
2020-02-29 23:34:20 +0100

    Add data-model and rendered items.

--- src/com/bmb/todo/app/Controller.java
+++ src/com/bmb/todo/app/Controller.java
@@ -1,4 +1,37 @@
 package com.bmb.todo.app;
 
+import com.bmb.todo.app.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+    private List<TodoItem> todoitems;
+
+    @FXML
+    private ListView todoListView;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Mail birthday card",
+                "Buy 30th birthday card for John", LocalDate.of(2020, Month.AUGUST, 26));
+
+        TodoItem item2 = new TodoItem("Doctors appointment",
+                "Go to doctor Marlon for the checkup", LocalDate.of(2020, Month.MAY, 3));
+
+        TodoItem item3 = new TodoItem("Working meeting",
+                "Meeting to discuss the begining of the sprint", LocalDate.of(2020, Month.JULY, 24));
+
+        todoitems = new ArrayList<TodoItem>();
+        todoitems.add(item1);
+        todoitems.add(item2);
+        todoitems.add(item3);
+
+        todoListView.getItems().setAll(todoitems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
 }
