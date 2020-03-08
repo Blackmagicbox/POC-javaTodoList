890314700c1ca033490c568eefc473e46275293a
Rafael <rafa@blackmagicbox.com.br>
2020-03-08 21:04:42 +0100

    Implement delete method [DRAFT]

--- src/com/bmb/todo/app/Controller.java
+++ src/com/bmb/todo/app/Controller.java
@@ -6,13 +6,19 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Optional;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
@@ -27,14 +33,26 @@ public class Controller {
   private TextArea todoListItemDetailsView;
   @FXML
   private Label deadlineLabel;
-
   @FXML
   private BorderPane mainBorderPane;
+  @FXML
+  private ContextMenu listContextMenu;
+
 
   /**
    * Initialize method.
    */
   public void initialize() {
+    listContextMenu = new ContextMenu();
+    MenuItem deleteMenuItem = new MenuItem("Delete");
+    deleteMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent actionEvent) {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+        deleteItem(item);
+      }
+    });
+    listContextMenu.getItems().addAll(deleteMenuItem);
     todoListView.getSelectionModel().selectedItemProperty()
         .addListener((observableValue, oldValue, newValue) -> {
           if (newValue != null) {
@@ -98,5 +116,19 @@ public class Controller {
   public void updateTodoItemText(TodoItem item) {
     todoListItemDetailsView.setText(item.getDetails());
     deadlineLabel.setText(item.getDeadLine().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+  }
+
+  public void deleteItem(TodoItem item) {
+    Alert alert = new Alert(AlertType.CONFIRMATION);
+    alert.setTitle("Delete Todo Item");
+    alert.setHeaderText("Delete items from todo list item");
+    alert.setContentText("Do you really want to delete: " + item.getShortDescription()
+        + " press Ok to confirm or Cancel to keep it");
+    Optional<ButtonType> result = alert.showAndWait();
+
+    if (result.isPresent() && (result.get() == ButtonType.OK)) {
+      TodoData.getInstance().deleteTodoItem(item);
+    }
+    System.out.println(item);
   }
 }
--- src/com/bmb/todo/app/datamodel/TodoData.java
+++ src/com/bmb/todo/app/datamodel/TodoData.java
@@ -81,4 +81,8 @@ public class TodoData {
   public void addTodoItem(TodoItem item) {
     todoItems.add(item);
   }
+
+  public void deleteTodoItem(TodoItem item) {
+    System.out.println(item);
+  }
 }
